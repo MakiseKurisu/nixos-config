3fbcde1a2009fa39491f3d8e922a6ada786c8e7f
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2023-09-18 21:50:00 +0800

    Update ThinkPad VFIO blacklist

--- modules/thinkpad.nix
+++ modules/thinkpad.nix
@@ -15,7 +15,12 @@
 
   boot = {
     extraModprobeConfig = ''
-      options vfio-pci ids=1c5c:1639,10de:1fb9,10de:10fa,10de:1e93,10de:10f8,10de:1ad8,10de:1ad9 disable_idle_d3=1
+      options vfio-pci ids=1c5c:1639 # Hynix NVMe SSD
+      options vfio-pci ids=10de:1fb9,10de:10fa # NVIDIA T1000
+      options vfio-pci ids=10de:1e93,10de:10f8,10de:1ad8,10de:1ad9 # NVIDIA GeForce RTX 2080 Max-Q 
+      options vfio-pci ids=10de:11fc,10de:0e0b # NVIDIA Quadro K2100M
+      options vfio-pci disable_idle_d3=1
+      options vfio-pci disable_vga=1
     '';
   };
 
