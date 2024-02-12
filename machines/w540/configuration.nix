753752789458221c39ee7c90799b747d517f7893
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-02-12 19:48:51 +0800

    fix: allow main and w540 to be built as flake

--- machines/w540/configuration.nix
+++ machines/w540/configuration.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
   imports = [
+    inputs.home-manager.nixosModules.home-manager
     ../../modules/base.nix
     ../../modules/desktop.nix
     ../../modules/podman.nix
