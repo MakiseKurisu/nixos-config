3472b9446a075f6846d35ec2bb71747d683180fe
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-02-24 22:05:27 +0800

    feat: use flake-parts to handle pre-system attributes

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       inputs.pre-commit-hooks.follows = "pre-commit-hooks";
     };
     flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
@@ -36,58 +37,79 @@
     , nixpkgs-unstable
     , devenv
     , flake-compat
+    , flake-parts
     , flake-utils
     , home-manager
     , nixos-vscode-server
     , NUR
     , pre-commit-hooks
     , ...
-    }: {
-      formatter = {
-        x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Run `nix fmt` to use this command
+        formatter = pkgs.nixpkgs-fmt;
       };
-      nixosConfigurations = {
-        app01 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/app01/configuration.nix
-          ];
-        };
-        main = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/main/configuration.nix
-          ];
-        };
-        nas = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/nas/configuration.nix
-          ];
-        };
-        p15 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/p15/configuration.nix
-          ];
-        };
-        rpi3 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/rpi3/configuration.nix
-          ];
-        };
-        w540 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          system = "x86_64-linux";
-          modules = [
-            ./machines/w540/configuration.nix
-          ];
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+
+        nixosConfigurations = {
+          app01 = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/app01/configuration.nix
+            ];
+          };
+          main = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/main/configuration.nix
+            ];
+          };
+          nas = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/nas/configuration.nix
+            ];
+          };
+          p15 = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/p15/configuration.nix
+            ];
+          };
+          rpi3 = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/rpi3/configuration.nix
+            ];
+          };
+          w540 = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = "x86_64-linux";
+            modules = [
+              ./machines/w540/configuration.nix
+            ];
+          };
         };
       };
     };
