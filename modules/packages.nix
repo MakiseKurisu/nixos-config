ea3551bec7d3e8188ea28612c89e1eb1fec94468
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-02-27 00:33:43 +0800

    fix: correctly handle dotnet version number
    See https://discourse.nixos.org/t/dotnet-maui-workload/20370/13

--- modules/packages.nix
+++ modules/packages.nix
@@ -18,6 +18,9 @@
           for i in $out/sdk/*
           do
             i=$(basename $i)
+            length=$(printf "%s" "$i" | wc -c)
+            substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+            i="$substring""00"
             mkdir -p $out/metadata/workloads/''${i/-*}
             touch $out/metadata/workloads/''${i/-*}/userlocal
           done
