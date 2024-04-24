f1b7e7c8a5ab87ddc4e91ee7fae9319ecf558a20
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-04-25 00:48:25 +0800

    refactor: single executable android-system-bin-wrapper

--- machines/davinci/android-system-bin-wrapper.nix
+++ machines/davinci/android-system-bin-wrapper.nix
@@ -1,26 +1,31 @@
 { stdenvNoCC
 , lib
 , bash
+, coreutils
 }:
 
 stdenvNoCC.mkDerivation rec {
   name = "android-system-bin-wrapper";
   installPhase = ''
     mkdir -p "$out/bin"
-    mapfile -t android_bin <"${./android-system-bin.list}"
-    for i in "''${android_bin[@]}"; do
-      cat <<EOF >"$out/bin/$i"
+
+    cat <<EOF >"$out/bin/android-system-bin-wrapper"
     #!${lib.getBin bash}/bin/bash
 
-    /android/system/bin/linker64 "/android/system/bin/$i" "\$@"
+    /android/system/bin/linker64 "/android/system/bin/\$(${lib.getBin coreutils}/bin/basename "\$0")" "\$@"
     EOF
-      chmod +x "$out/bin/$i"
+    chmod +x "$out/bin/android-system-bin-wrapper"
+
+    mapfile -t android_bin <"${./android-system-bin.list}"
+    for i in "''${android_bin[@]}"; do
+      ln -s android-system-bin-wrapper "$out/bin/$i"
     done
   '';
   dontUnpack = true;
 
   buildInputs = [
     bash
+    coreutils
   ];
 
   meta = {
