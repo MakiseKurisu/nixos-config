d9212ae0dc7b4475eb2b688a7776be525b4850f0
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-10-03 01:03:30 +0800

    fix: allow smb access from WireGuard

--- modules/file-share.nix
+++ modules/file-share.nix
@@ -18,7 +18,7 @@
         #use sendfile = yes
         #max protocol = smb2
         # note: localhost is the ipv6 localhost ::1
-        hosts allow = 192.168.9. 127.0.0.1 localhost
+        hosts allow = 192.168.9. 10.0.32. 127.0.0.1 localhost
         hosts deny = 0.0.0.0/0
         guest account = nobody
         map to guest = bad user
