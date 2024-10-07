01d003c325ad8eed05cf5a702568a6233c9e4bc9
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-10-08 01:14:49 +0800

    fix: no longer need to manually load wwan driver

--- machines/openwrt/networks/wwan0.nix
+++ machines/openwrt/networks/wwan0.nix
@@ -7,24 +7,8 @@
   ];
 
   etc = {
-    "rc.local".text = ''
-      sh /etc/wwan/reload_driver
-    '';
-    "wwan/reload_driver".text = ''
+    "wwan/keep_alive".text = ''
       #!/bin/sh
-      qmi_count="$(grep Driver=qmi_wwan /sys/kernel/debug/usb/devices | wc -l)"
-      if [ "$qmi_count" != "1" ]; then
-        logger -st "$(basename "$0")" -p "daemon.notice" "Detected incorrect number of qmi_wwan devices: $qmi_count"
-        rmmod option
-        rmmod qmi_wwan
-      fi
-      modprobe option
-      modprobe qmi_wwan
-
-      # https://whrl.pl/Rgk1Lv
-      echo 2dee 4d22 ff 2001 7d04 > /sys/bus/usb-serial/drivers/option1/new_id
-      echo 2dee 4d22 >/sys/bus/usb/drivers/qmi_wwan/new_id
-
       . /usr/share/libubox/jshn.sh
       json_load "$(ubus call network.interface.wwan0 status)"
       json_get_var up up
@@ -33,7 +17,7 @@
       fi
     '';
     "crontabs/root".text = ''
-      * * * * * sh /etc/wwan/reload_driver
+      * * * * * sh /etc/wwan/keep_alive
     '';
   };
 
