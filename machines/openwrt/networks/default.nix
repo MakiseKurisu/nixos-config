819d85509f74ec7706fee8cf664f95e58c738ef7
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-10-13 01:20:05 +0800

    fix: update VLAN for pve11

--- machines/openwrt/networks/default.nix
+++ machines/openwrt/networks/default.nix
@@ -17,24 +17,24 @@
           {
             name = "br-lan";
             type = "bridge";
-            ports = ["eth1" "wan" "lan1" "lan2" "lan3" "lan4"];
+            ports = ["eth1" "lan1" "lan2" "lan3" "lan4" "wan"];
           }
         ];
         bridge-vlan = [
           {
             device = "br-lan";
             vlan = 10;
-            ports = ["eth1" "wan" "lan1:t"];
+            ports = ["eth1" "lan1:t" "lan2:t" "wan"];
           }
           {
             device = "br-lan";
             vlan = 20;
-            ports = ["lan1:t" "lan2" "lan3" "lan4"];
+            ports = ["lan1:t" "lan2:t" "lan3" "lan4"];
           }
           {
             device = "br-lan";
             vlan = 30;
-            ports = ["lan1:t"];
+            ports = ["lan1:t" "lan2:t"];
           }
         ];
       };
