25105d3e3a835df003b3f8ed46e449fb25d5907a
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2024-10-22 21:58:56 +0800

    feat: add builder module

---
+++ modules/builder.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        supportedFeatures = [
+          "kvm"
+          "big-parallel"
+        ];
+        sshUser = "excalibur";
+        protocol = "ssh-ng";
+        hostName = "app01.protoducer.com";
+      }
+    ];
+  };
+}
