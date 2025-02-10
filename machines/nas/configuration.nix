eed459bf5eae10c8045642d05b40e48487250a35
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-02-10 16:33:06 +0800

    refactor: move app01 services to a dedicated nas machine

--- machines/nas/configuration.nix
+++ machines/nas/configuration.nix
@@ -13,23 +13,23 @@
     ../../modules/packages-base.nix
     ../../modules/users-base.nix
     #../../modules/vfio.nix
-    #../../modules/virtualization.nix
+    ../../modules/virtualization-base.nix
 
     #../../modules/nfs-nas.nix
-    ../../modules/nfs-app01.nix
+    # ../../modules/nfs-app01.nix
     #../../modules/wireguard.nix
+
+    ../../modules/pr/fastapi-dls.nix
+    ../../modules/pr/aria2.nix
 
     ./hardware-configuration.nix
   ];
 
-  boot = {
-    kernelParams = [
-      "console=ttyS0"
-    ];
-    loader.efi.efiSysMountPoint = "/boot/efi";
-  };
-
   services = {
+    # Must configure to NOT listen on 0.0.0.0:53 but 192.168.xxx.yyy:53
+    # As systemd-resolved would listen on 127.0.0.53:53
+    technitium-dns-server.enable = true;
+
     nfs.server = {
       exports = ''
         /media         192.168.9.0/24(rw,fsid=0,no_subtree_check)
@@ -50,6 +50,7 @@
           "directory mask" = "0755";
           "force user" = "excalibur";
           "force group" = "users";
+          "acl allow execute always" = "yes";
         };
         raid = {
           path = "/media/raid";
@@ -60,6 +61,276 @@
           "directory mask" = "0755";
           "force user" = "excalibur";
           "force group" = "users";
+          "acl allow execute always" = "yes";
+        };
+      };
+    };
+
+    mihomo = {
+      enable = true;
+      tunMode = true;
+      configFile = "/var/lib/mihomo/clash.yaml";
+      webui = pkgs.fetchzip {
+        url = "https://github.com/MetaCubeX/metacubexd/releases/download/v1.176.2/compressed-dist.tgz";
+        stripRoot = false;
+        hash = "sha256-rOeeoSSDPsvMqhJAUrADzsWvdlhJnRYGoHHPE+8x/zc=";
+      };
+    };
+
+    iperf3 = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    aria2 = {
+      enable = true;
+      rpcSecretFile = pkgs.writeText "aria2-rpc-token.txt" "P3TERX";
+      openPorts = true;
+      serviceUMask = "0007";
+      settings = {
+        dir = "/srv/aria2";
+        rpc-listen-all = true;
+        input-file = "/var/lib/aria2/aria2.session";
+        continue = true;
+        max-concurrent-downloads = 100;
+        max-overall-upload-limit = "64K";
+        max-connection-per-server = 10;
+        http-accept-gzip = true;
+        file-allocation = "falloc";
+        save-session-interval = 10;
+        bt-prioritize-piece = "head,tail";
+        bt-remove-unselected-file = true;
+        seed-ratio = 0.1;
+        seed-time = 0;
+      };
+    };
+
+    jellyfin.enable = true;
+
+    pykms = {
+      enable = true;
+      listenAddress = "::";
+      openFirewallPort = true;
+    };
+
+    home-assistant = {
+      enable = true;
+      extraPackages = python3Packages: with python3Packages; [
+        gtts
+      ];
+      extraComponents = [
+        "default_config"
+        "met"
+        "mobile_app"
+        "radio_browser"
+        "tuya"
+        "xiaomi_miio"
+      ];
+      config = {
+        http = {
+          use_x_forwarded_for = true;
+          trusted_proxies = [ "127.0.0.1" ];
+        };
+        homeassistant = {
+          unit_system = "metric";
+          name = "Home";
+        };
+        mobile_app = {};
+      };
+    };
+
+    fastapi-dls = {
+      enable = true;
+      package = pkgs.pr-fastapi-dls.fastapi-dls;
+      listenPort = 8001;
+      dlsAddress = "dls.protoducer.com";
+      dlsPort = 443;
+    };
+
+    nginx = {
+      enable = true;
+      recommendedOptimisation = true;
+      recommendedTlsSettings = true;
+      recommendedBrotliSettings = true;
+      recommendedGzipSettings = true;
+      recommendedZstdSettings = true;
+      recommendedProxySettings = true;
+      virtualHosts = let
+        https = host: host // {
+          sslCertificate = "/var/lib/nginx/cert.pem";
+          sslCertificateKey = "/var/lib/nginx/key.pem";
+          forceSSL = true;
+          kTLS = true;
+        };
+        http = host: host // {
+          rejectSSL = true;
+        }; in {
+        "_" = https { locations."/".return = "404"; };
+        "apt.protoducer.com" = http { locations."/".proxyPass = "http://127.0.0.1:3142/"; };
+        "aria.protoducer.com" = http {
+          locations = {
+            "/".root = "${pkgs.ariang}/share/ariang/";
+            "/jsonrpc" = {
+              proxyPass = "http://127.0.0.1:6800/jsonrpc";
+              extraConfig = ''
+                add_header 'Access-Control-Allow-Origin' '*' always;
+              '';
+            };
+          };
+        };
+        "dns.protoducer.com" = https { locations."/".proxyPass = "http://127.0.0.1:5380/"; };
+        "jf.protoducer.com" = https {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://127.0.0.1:8096/";
+          };
+        };
+        "proxy.protoducer.com" = https {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://127.0.0.1:9090/";
+          };
+        };
+        "ha.protoducer.com" = http {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://127.0.0.1:8123/";
+          };
+        };
+        "dls.protoducer.com" = https { locations."/".proxyPass = "https://127.0.0.1:8001/"; };
+      };
+    };
+  };
+
+  networking.firewall.allowedUDPPorts = [
+    53 # technitium-dns-server
+  ];
+  networking.firewall.allowedTCPPorts = [
+    53 # technitium-dns-server
+    80 # nginx
+    443 # nginx
+    9090 # mihomo
+  ];
+  networking.firewall.allowedTCPPortRanges = [
+    # mihomo
+    {
+      from = 7890;
+      to = 7894;
+    }
+  ];
+
+  systemd.services = {
+    apt-cacher-ng = {
+      script = ''
+        set -eu
+        ${lib.getExe pkgs.apt-cacher-ng} -c ${pkgs.writeTextDir "acng.conf" (lib.readFile ../../configs/acng/acng.conf)} \
+          "SupportDir=${pkgs.apt-cacher-ng}/lib/apt-cacher-ng/" \
+          "LocalDirs=acng-doc ${pkgs.apt-cacher-ng}/share/doc/apt-cacher-ng/"
+      '';
+      serviceConfig = {
+        Type = "exec";
+        DynamicUser = true;
+        CacheDirectory = "apt-cacher-ng";
+        LogsDirectory = "apt-cacher-ng";
+        RuntimeDirectory = "apt-cacher-ng";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+
+  networking.interfaces.eno1.useDHCP = false;
+  networking.interfaces.eno2.useDHCP = false;
+  systemd.network = {
+    netdevs = {
+       "20-br0" = {
+         netdevConfig = {
+           Kind = "bridge";
+           Name = "br0";
+         };
+        bridgeConfig = {
+          STP = true;
+          VLANFiltering = true;
+        };
+       };
+    };
+    networks = {
+      "30-eno1" = {
+        matchConfig.Name = "eno1";
+        networkConfig.Bridge = "br0";
+        linkConfig.RequiredForOnline = "enslaved";
+        bridgeVLANs = [
+          {
+            PVID = 1;
+            EgressUntagged = 1;
+          }
+          {
+            VLAN = 10;
+          }
+          {
+            VLAN = 20;
+          }
+          {
+            VLAN = 30;
+          }
+        ];
+      };
+      "30-eno2" = {
+        matchConfig.Name = "eno2";
+        networkConfig.Bridge = "br0";
+        linkConfig.RequiredForOnline = "enslaved";
+        bridgeVLANs = [
+          {
+            PVID = 1;
+            EgressUntagged = 1;
+          }
+          {
+            VLAN = 10;
+          }
+          {
+            VLAN = 20;
+          }
+          {
+            VLAN = 30;
+          }
+        ];
+      };
+      "40-br0" = {
+        matchConfig.Name = "br0";
+        networkConfig = {
+          LinkLocalAddressing = false;
+        };
+        bridgeConfig = {};
+        bridgeVLANs = [
+          {
+            PVID = 1;
+            EgressUntagged = 1;
+          }
+          {
+            VLAN = 10;
+          }
+          {
+            VLAN = 20;
+          }
+          {
+            VLAN = 30;
+          }
+        ];
+        vlan = [
+          "vlan20"
+        ];
+        linkConfig = {
+          RequiredForOnline = false;
+        };
+      };
+      "50-vlan20" = {
+        matchConfig.Name = "vlan20";
+        networkConfig = {
+          DHCP = true;
+          Domains = "protoducer.com vamrs.org";
+          Address = "192.168.9.3/24";
+        };
+        linkConfig = {
+          RequiredForOnline = true;
         };
       };
     };
@@ -72,23 +343,23 @@
       ];
   };
 
-  systemd.services.duplicacy = {
-    environment = {
-      https_proxy = "socks5://10.0.20.1:1080";
-    };
-    script = ''
-      set -eu
-      ${pkgs.duplicacy}/bin/duplicacy copy -from local -to gcd -threads 2
-    '';
-    serviceConfig = {
-      Type = "exec";
-      User = "excalibur";
-      Group = "users";
-      WorkingDirectory = "~";
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
+  # systemd.services.duplicacy = {
+  #   environment = {
+  #     https_proxy = "socks5://10.0.20.1:1080";
+  #   };
+  #   script = ''
+  #     set -eu
+  #     ${pkgs.duplicacy}/bin/duplicacy copy -from local -to gcd -threads 2
+  #   '';
+  #   serviceConfig = {
+  #     Type = "exec";
+  #     User = "excalibur";
+  #     Group = "users";
+  #     WorkingDirectory = "~";
+  #   };
+  #   wantedBy = [ "multi-user.target" ];
+  # };
 
   networking.hostName = "nas";
-  system.stateVersion = "22.11";
+  system.stateVersion = "24.11";
 }
