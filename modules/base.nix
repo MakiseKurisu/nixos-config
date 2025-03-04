70121ec6f3eb16f7647b68f58cc7ad5a86b74182
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-03-05 01:02:39 +0800

    feat: add extra nix binary cache

--- modules/base.nix
+++ modules/base.nix
@@ -46,6 +46,7 @@
       substituters = lib.mkBefore [
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
       ];
       trusted-users = [
         "root"
