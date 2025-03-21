1c33cc5ce8df9ac9dd9ffc4ac4a2ea66d7899e1d
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-03-21 21:47:50 +0800

    eat: optimize network parameters

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -3,11 +3,29 @@
 {
   boot = {
     kernelPackages = lib.mkOverride 990 pkgs.unstable.linuxPackages;
+    kernelModules = [ "tcp_bbr" ];
     kernelParams = [
       "console=tty1"
     ];
     kernel.sysctl = {
       "kernel.dmesg_restrict" = 1;
+      "net.ipv4.tcp_fastopen" = 3;
+      "net.ipv4.tcp_keepalive_time" = 60;
+      "net.ipv4.tcp_keepalive_intvl" = 10;
+      "net.ipv4.tcp_keepalive_probes" = 6;
+      "net.ipv4.tcp_mtu_probing" = 1;
+      "net.core.default_qdisc" = "cake";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.conf.all.rp_filter" = 2;
+      "net.ipv4.conf.default.rp_filter" = 2;
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.all.secure_redirects" = 0;
+      "net.ipv4.conf.default.secure_redirects" = 0;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+      "net.ipv4.icmp_echo_ignore_all" = 0;
+      "net.ipv6.icmp.echo_ignore_all" = 0;
     };
     loader = {
       efi = {
