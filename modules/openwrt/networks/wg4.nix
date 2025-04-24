9a5bde331c8048b8506d1c1118a3df647a532b79
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-04-24 23:54:02 +0800

    fix: add tunnel to sh.breadro.com

--- modules/openwrt/networks/wg4.nix
+++ modules/openwrt/networks/wg4.nix
@@ -19,13 +19,22 @@
             private_key._secret = "wg4_private_key";
           };
         };
-        wireguard_wg4 = [{
-          allowed_ips = [ "10.1.0.1/24" "fdbe::1/64" ];
-          description = "breadro";
-          endpoint_host = "breadro.com";
-          public_key = "A1Q1YBomxVI8r+hrHcw667WsOCqhkeKXX17YkbpQWn0=";
-          route_allowed_ips = 1;
-        }];
+        wireguard_wg4 = [
+          {
+            allowed_ips = [ "10.1.0.1/32" "fdbe::1/128" ];
+            description = "breadro";
+            endpoint_host = "breadro.com";
+            public_key = "A1Q1YBomxVI8r+hrHcw667WsOCqhkeKXX17YkbpQWn0=";
+            route_allowed_ips = 1;
+          }
+          {
+            allowed_ips = [ "10.1.0.4/32" "fdbe::4/128" ];
+            description = "sh.breadro";
+            endpoint_host = "sh.breadro.com";
+            public_key = "5FmrArolI4v240v5MnMA7XmYXjSFDmIhtT3GmmxKwiY=";
+            route_allowed_ips = 1;
+          }
+        ];
       };
     };
   };
