3fece09d787608f8f4fc5c685c40f0567a0f2a8f
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-06-04 17:31:15 +0800

    feat: additional MIME associations

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -370,6 +370,18 @@
             "x-scheme-handler/baiduyunguanjia" = [ "baidunetdisk.desktop" ];
             "x-scheme-handler/msteams" = [ "teams-for-linux.desktop" ];
             "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/mid" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/news" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/snews" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/nntp" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/feed" = [ "thunderbird.desktop" ];
+            "application/rss+xml" = [ "thunderbird.desktop" ];
+            "application/x-extension-rss" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/webcal" = [ "thunderbird.desktop" ];
+            "x-scheme-handler/webcals" = [ "thunderbird.desktop" ];
+            "message/rfc822" = [ "thunderbird.desktop" ];
+            "text/calendar" = [ "thunderbird.desktop" ];
+            "application/x-extension-ics" = [ "thunderbird.desktop" ];
           };
         };
       };
