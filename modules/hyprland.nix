883911dcec7ce40eb9c0f9828d08d073c7350bdc
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-07-17 16:09:47 +0800

    fmt: use nested structure

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -328,12 +328,14 @@
           ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
           ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
         ];
-        "plugin:touch_gestures" = {
-          hyprgrass-bind = [
-            ", tap:3, sendshortcut, mouse:274"
-            ", swipe:3:l, sendshortcut, mouse:275"
-            ", swipe:3:r, sendshortcut, mouse:276"
-          ];
+        plugin = {
+          touch_gestures = {
+            hyprgrass-bind = [
+              ", tap:3, sendshortcut, mouse:274"
+              ", swipe:3:l, sendshortcut, mouse:275"
+              ", swipe:3:r, sendshortcut, mouse:276"
+            ];
+          };
         };
         source = [
           "~/.config/hypr/machine.conf"
