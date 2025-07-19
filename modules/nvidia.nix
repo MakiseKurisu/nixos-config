2cac7f1dea741c03f0d499a77032cc6ec7bfa15d
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-07-19 19:25:42 +0800

    chore: clean up nvidia.nix

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,33 +1,15 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot = {
-    blacklistedKernelModules = [ "nouveau" ];
-  };
-
-  environment = {
-    systemPackages =
-      let
-        nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-          export __NV_PRIME_RENDER_OFFLOAD=1
-          export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-          export __GLX_VENDOR_LIBRARY_NAME=nvidia
-          export __VK_LAYER_NV_optimus=NVIDIA_only
-          exec "$@"
-        '';
-      in
-      with pkgs; [
-        nvidia-offload
-      ];
-  };
-
   hardware = {
     nvidia = {
       open = lib.mkDefault true;
       modesetting.enable = true;
       prime = {
-        reverseSync.enable = true;
-        offload.enable = true;
+        offload ={
+          enable = true;
+          enableOffloadCmd = true;
+        };
       };
     };
   };
@@ -45,6 +27,9 @@
   };
 
   services = {
-    xserver.videoDrivers = [ "nvidia" ];
+    xserver.videoDrivers = [ 
+      "modesetting"
+      "nvidia"
+    ];
   };
 }
