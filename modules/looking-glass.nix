65e69ff9421b1a6fdf5d6fc4fee03cf46b05effb
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-07-24 14:46:27 +0800

    refactor: move looking-glass to its own module

---
+++ modules/looking-glass.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [
+      kvmfr
+    ];
+    extraModprobeConfig = ''
+      options kvmfr static_size_mb=128
+    '';
+    kernelModules = [
+      "kvmfr"
+    ];
+  };
+
+  environment = {
+    systemPackages =
+      with pkgs; [
+        looking-glass-client
+      ];
+  };
+
+  services = {
+    udev.extraRules = ''
+      SUBSYSTEM=="kvmfr", OWNER="excalibur", GROUP="libvirtd", MODE="0660"
+    '';
+  };
+
+  programs.virt-manager.enable = true;
+
+  virtualisation.libvirtd = {
+    qemu = {
+      verbatimConfig = ''
+        cgroup_device_acl = [
+            "/dev/null", "/dev/full", "/dev/zero",
+            "/dev/random", "/dev/urandom",
+            "/dev/ptmx", "/dev/kvm",
+            "/dev/kvmfr0"
+        ]
+      '';
+    };
+  };
+}
--- modules/vfio.nix
+++ modules/vfio.nix
@@ -1,30 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot = {
-    extraModulePackages = with config.boot.kernelPackages; [
-      kvmfr
-    ];
-    extraModprobeConfig = ''
-      options kvmfr static_size_mb=128
-    '';
-    kernelModules = [
-      "kvmfr"
-    ];
-  };
-
   environment = {
     systemPackages =
       with pkgs; [
         virt-viewer
-        unstable.looking-glass-client
       ];
-  };
-
-  services = {
-    udev.extraRules = ''
-      SUBSYSTEM=="kvmfr", OWNER="excalibur", GROUP="libvirtd", MODE="0660"
-    '';
   };
 
   programs.virt-manager.enable = true;
@@ -41,14 +22,6 @@
       vhostUserPackages = with pkgs; [
         virtiofsd
       ];
-      verbatimConfig = ''
-        cgroup_device_acl = [
-            "/dev/null", "/dev/full", "/dev/zero",
-            "/dev/random", "/dev/urandom",
-            "/dev/ptmx", "/dev/kvm",
-            "/dev/kvmfr0"
-        ]
-      '';
     };
   };
 }
