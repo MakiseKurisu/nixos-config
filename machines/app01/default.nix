fa11113a49165ba499412200992b657a98467594
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-08-03 21:58:42 +0800

    feat: add modem loss recovery

--- machines/app01/default.nix
+++ machines/app01/default.nix
@@ -240,6 +240,24 @@
         fi
         ${lib.getExe config.virtualisation.incus.package} start local:openwrt
       ''}"
+
+      # Attempt to recovery disconnected wwan device
+      ACTION=="remove", SUBSYSTEM=="usbmisc", ENV{DEVNAME}=="/dev/cdc-wdm0", ENV{ID_MM_CANDIDATE}=="1", RUN+="${pkgs.writeShellScript "restart-machine" ''
+        ${lib.getExe' pkgs.util-linux "logger"} -s -t "restart-machine" "Detect wwan disconnection."
+        i=0
+        while sleep 1; do
+          i=$(( i + 1))
+          if [[ -c /dev/cdc-wdm0 ]]; then
+            ${lib.getExe' pkgs.util-linux "logger"} -s -t "restart-machine" "wwan reconnected in $i seconds."
+            break
+          fi
+          if (( i >= 60 )); then
+            ${lib.getExe' pkgs.util-linux "logger"} -s -t "restart-machine" "wwan did not recover in time. Trigger reboot."
+            systemctl reboot
+            break
+          fi
+        done
+      ''}"
     '';
 
     pico-remote-play-assistant = {
