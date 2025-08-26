dd1cac62569b26c784f1b147cce53ca48f9659f7
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-08-26 14:46:18 +0800

    chore: update flake inputs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs-droid";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-on-droid = {
