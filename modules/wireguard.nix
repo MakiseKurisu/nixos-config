2293b492262f5504e9474dff3f6f7cf4c7669144
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-11-26 19:52:42 +0800

    fix: switch to wg-quick implementation
    This implementation is better with DNS and default route.

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -2,33 +2,10 @@
 
 {
   networking = {
-    #nameservers = lib.mkBefore [ "192.168.11.1" "fd11::1" "114.114.114.114" ];
-    wireguard.interfaces = {
-      /*
+    wg-quick.interfaces = {
       wg0 = {
-        ips = [ "10.0.1.10/32" "fd01::10/128" ];
-        mtu = 1280;
-        privateKeyFile = "/home/excalibur/wireguard/wg0.key";
-
-        peers = [
-          {
-            publicKey = "Incx4TYB5iWaao0gHSsUlUSGxHF4ItiaspZexMa8v30=";
-
-            allowedIPs = [ 
-              "10.0.1.0/24"
-              "fd01::/64"
-              "192.168.11.0/24"
-              "fd11::/64"
-            ];
-
-            endpoint = "protoducer-cn-wuh.64-b.it:51820";
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-      */
-      wg1 = {
-        ips = [ "10.0.32.3/32" "fd32::3/128" ];
+        address = [ "10.0.32.3/32" "fd32::3/128" ];
+        dns = [ "10.0.32.1" ];
         mtu = 1280;
         privateKeyFile = "/home/excalibur/wireguard/wg1.key";
 
@@ -54,26 +31,6 @@
           }
         ];
       };
-      /*
-      wg2 = {
-        ips = [ "10.0.20.4/32" "fd20::4/128" ];
-        mtu = 1280;
-        privateKeyFile = "/home/excalibur/wireguard/wg2.key";
-
-        peers = [
-          {
-            publicKey = "WVcdwHDpBQq2bg4bJE6zHRdWuPG7mptkuF48HxNFNw4=";
-            allowedIPs = [
-              "10.0.20.0/24"
-              "fd20::/64"
-            ];
-
-            endpoint = "vamrs.vpndns.net:51820";
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-      */
     };
   };
 }
