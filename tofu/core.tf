1ffe10929af868e821a7c100d3259d64e9b13159
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-12-09 01:30:44 +0800

    chore: update infra definition

--- tofu/core.tf
+++ tofu/core.tf
@@ -5,6 +5,8 @@ locals {
   amd1_name   = "amd1"
   arm0_name   = "arm0"
   arm1_name   = "arm1"
+  arm0_name2  = "arm0-2"
+  arm1_name2  = "arm1-2"
   if0_name    = "eth0"
   if1_name    = "eth1"
 }
@@ -62,26 +64,6 @@ resource oci_core_security_list security_list {
   ingress_security_rules {
     protocol = "58"
     source   = "::/0"
-  }
-
-  ingress_security_rules {
-    protocol = "6"
-    source   = "0.0.0.0/0"
-
-    tcp_options {
-      max = "51820"
-      min = "51820"
-    }
-  }
-
-  ingress_security_rules {
-    protocol = "6"
-    source   = "::/0"
-
-    tcp_options {
-      max = "51820"
-      min = "51820"
-    }
   }
 
   ingress_security_rules {
@@ -158,6 +140,43 @@ resource oci_core_instance amd0 {
   }
 }
 
+resource oci_core_instance amd1 {
+  availability_domain = local.availability_domain.name
+  compartment_id      = local.tenancy_ocid
+  shape               = "VM.Standard.E2.1.Micro"
+  display_name        = local.amd1_name
+  is_pv_encryption_in_transit_enabled = true
+
+  metadata = {
+    ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
+  }
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet.id
+    assign_ipv6ip    = true
+    assign_private_dns_record = true
+    assign_public_ip = true
+    display_name     = local.if0_name
+    hostname_label   = local.amd1_name
+    private_ip       = "${substr(oci_core_subnet.subnet.cidr_block, 0, length(oci_core_subnet.subnet.cidr_block) - length("0/24"))}11"
+    ipv6address_ipv6subnet_cidr_pair_details {
+      ipv6subnet_cidr = oci_core_subnet.subnet.ipv6cidr_blocks[0]
+      ipv6address = "${substr(oci_core_subnet.subnet.ipv6cidr_blocks[0], 0, length(oci_core_subnet.subnet.ipv6cidr_blocks[0]) - length("0000/64"))}11"
+    }
+  }
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = true
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = local.ubuntu_images.amd64.id
+    boot_volume_size_in_gbs = 50
+    boot_volume_vpus_per_gb = 120
+  }
+}
+
 resource oci_core_instance arm0 {
   availability_domain = local.availability_domain.name
   compartment_id      = local.tenancy_ocid
@@ -200,6 +219,24 @@ resource oci_core_instance arm0 {
   }
 }
 
+resource "oci_core_vnic_attachment" arm0_eth1 {
+  instance_id  = oci_core_instance.arm0.id
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet.id
+    assign_ipv6ip    = true
+    assign_private_dns_record = true
+    assign_public_ip = true
+    display_name     = local.if1_name
+    hostname_label   = local.arm0_name2
+    private_ip       = "${substr(oci_core_subnet.subnet.cidr_block, 0, length(oci_core_subnet.subnet.cidr_block) - length("0/24"))}30"
+    ipv6address_ipv6subnet_cidr_pair_details {
+      ipv6_subnet_cidr = oci_core_subnet.subnet.ipv6cidr_blocks[0]
+      ipv6_address = "${substr(oci_core_subnet.subnet.ipv6cidr_blocks[0], 0, length(oci_core_subnet.subnet.ipv6cidr_blocks[0]) - length("0000/64"))}30"
+    }
+  }
+}
+
 resource oci_core_instance arm1 {
   availability_domain = local.availability_domain.name
   compartment_id      = local.tenancy_ocid
@@ -239,5 +276,23 @@ resource oci_core_instance arm1 {
     source_id   = local.ubuntu_images.arm64.id
     boot_volume_size_in_gbs = 50
     boot_volume_vpus_per_gb = 120
+  }
+}
+
+resource "oci_core_vnic_attachment" arm1_eth1 {
+  instance_id  = oci_core_instance.arm1.id
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet.id
+    assign_ipv6ip    = true
+    assign_private_dns_record = true
+    assign_public_ip = true
+    display_name     = local.if1_name
+    hostname_label   = local.arm1_name2
+    private_ip       = "${substr(oci_core_subnet.subnet.cidr_block, 0, length(oci_core_subnet.subnet.cidr_block) - length("0/24"))}31"
+    ipv6address_ipv6subnet_cidr_pair_details {
+      ipv6_subnet_cidr = oci_core_subnet.subnet.ipv6cidr_blocks[0]
+      ipv6_address = "${substr(oci_core_subnet.subnet.ipv6cidr_blocks[0], 0, length(oci_core_subnet.subnet.ipv6cidr_blocks[0]) - length("0000/64"))}31"
+    }
   }
 }
