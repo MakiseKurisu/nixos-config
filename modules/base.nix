e5dd0e2be7d4b5d5922158df74622c40b14656f2
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-12-17 01:35:46 +0800

    feat: add nix-community cache

--- modules/base.nix
+++ modules/base.nix
@@ -42,7 +42,10 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
-        "https://radxa.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       http2 = false;
     };
