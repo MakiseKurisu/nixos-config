7ab7155935a3c8ba4defc69dfb5abe7c3f02bf00
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-12-24 11:18:15 +0800

    fix: update remote builder settings

--- modules/zone/home.nix
+++ modules/zone/home.nix
@@ -9,42 +9,28 @@
         protocol = "ssh-ng";
         speedFactor = 32;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "cuda" "big-parallel" "kvm" ];
-        systems = [ "x86_64-linux" "aarch64-linux" ];
+        systems = [ "i686-linux" "x86_64-linux" "aarch64-linux" ];
       }
       {
         hostName = "p15";
         protocol = "ssh-ng";
         speedFactor = 12;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "kvm" ];
-        systems = [ "x86_64-linux" ];
+        systems = [ "i686-linux" "x86_64-linux" ];
       }
       {
         hostName = "nas";
         protocol = "ssh-ng";
         speedFactor = 6;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "kvm" ];
-        systems = [ "x86_64-linux" ];
+        systems = [ "i686-linux" "x86_64-linux" ];
       }
     ]);
-  };
-
-  programs = {
-    ssh = {
-      extraConfig = ''
-        Host main
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host p15
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host nas
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host *
-      '';
-    };
   };
 }
--- modules/zone/office.nix
+++ modules/zone/office.nix
@@ -9,22 +9,25 @@
         protocol = "ssh-ng";
         speedFactor = 20;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "cuda" "big-parallel" "kvm" ];
-        systems = [ "x86_64-linux" "aarch64-linux" ];
+        systems = [ "i686-linux" "x86_64-linux" "aarch64-linux" ];
       }
       {
         hostName = "b490";
         protocol = "ssh-ng";
         speedFactor = 4;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "kvm" ];
-        systems = [ "x86_64-linux" ];
+        systems = [ "i686-linux" "x86_64-linux" ];
       }
       {
         hostName = "orion-o6n";
         protocol = "ssh-ng";
         speedFactor = 12;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "big-parallel" "kvm" ];
         systems = [ "aarch64-linux" ];
       }
@@ -33,29 +36,10 @@
         protocol = "ssh-ng";
         speedFactor = 8;
         sshUser = "excalibur";
+        sshKey = "/home/excalibur/.ssh/id_rsa";
         supportedFeatures = [ "kvm" ];
         systems = [ "aarch64-linux" ];
       }
     ]);
-  };
-
-  programs = {
-    ssh = {
-      extraConfig = ''
-        Host yuntian
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host b490
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host orion-o6n
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host mac01
-          IdentityFile /home/excalibur/.ssh/id_rsa
-          IdentityFile ~/.ssh/id_rsa
-        Host *
-      '';
-    };
   };
 }
