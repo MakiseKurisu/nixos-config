6b0b8826a7720a982eb083573a5709ea0fbf4fa0
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2026-07-28 18:20:02 +0800

    fix: update dotnet-combined package

--- modules/packages.nix
+++ modules/packages.nix
@@ -12,36 +12,12 @@
 
   environment =
     let
-      dotnet-combined =
-        (
-          with pkgs.dotnetCorePackages;
-          combinePackages [
-            sdk_8_0
-          ]
-        ).overrideAttrs
-          (
-            finalAttrs: previousAttrs: {
-              # This is needed to install workload in $HOME
-              # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
-
-              postBuild = (previousAttrs.postBuild or "") + ''
-
-                for i in $out/sdk/*
-                do
-                  i=$(basename $i)
-                  length=$(printf "%s" "$i" | wc -c)
-                  substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
-                  i="$substring""00"
-                  mkdir -p $out/metadata/workloads/''${i/-*}
-                  touch $out/metadata/workloads/''${i/-*}/userlocal
-                done
-              '';
-            }
-          );
+      dotnet-combined = (with pkgs.dotnetCorePackages; combinePackages [ dotnet_10.sdk ]);
     in
     {
       sessionVariables = {
-        DOTNET_ROOT = "${dotnet-combined}";
+        DOTNET_PATH = "${dotnet-combined}/bin/dotnet";
+        DOTNET_ROOT = "${dotnet-combined}/share/dotnet";
       };
       systemPackages =
         let
